b8275a669f507cd50d9ae70d8ccde46558c7be95
rubenbranco <ruben.branco@outlook.pt>
2017-10-31 12:06:02 +0000

    Cenario2

--- código/index.js
+++ código/index.js
@@ -15,7 +15,14 @@ function main(){
             $(".container").append('<img src="./assets/imgs/' + order[i] + '.jpg" id="' + order[i] + '" height="300px" width="300px">');
             $("#" + order[i]).offset({left:$("#" + order[i - 2]).offset().left, top:$("#" + order[i - 2]).offset().top + $("#" + order[i - 2]).height()});
         }
-        $("#" + order[i]).hover(function(){$(this).css({'border': '3px solid red', 'animation':'border-pulse 3s infinite'})}, function(){$(this).css({'border':'none', 'animation':'none'})});
+        $("#" + order[i]).hover(function(){
+            $(this).css({'border': '3px solid red', 'animation':'border-pulse 3s infinite', 'cursor':'pointer'});
+            $("body").append('<p style="font-size:40px; z-index:10; animation:text-pulsate 3s infinite;" id="' + $(this).prop('id') + '-text">' + $(this).prop('id') + '</p>');
+            $("#" + $(this).prop('id') + '-text').offset({left:$(this).offset().left + $(this).width() * 0.30, top:$(this).offset().top + $(this).height() * 0.30})
+        }, function(){
+            $(this).css({'border':'none', 'animation':'none'});
+            $("#" + $(this).prop('id') + '-text').remove();
+        });
     }
 }
 
