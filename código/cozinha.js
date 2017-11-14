c339f38e1d546ef98e96bf1cbc3828f68a0c7f75
rubenbranco <ruben.branco@outlook.pt>
2017-11-14 14:30:13 +0000

    code

--- código/cozinha.js
+++ código/cozinha.js
@@ -16,7 +16,24 @@ function main(){
         date.setSeconds(date.getSeconds() + 1);
         updateTimeUI();
     }, 1000);
-
+    $("#loica").click(function(){
+        $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Máquina Lavar Loiça' +
+            '</h1><i class="material-icons close" id="popup-close">' +
+                    'cancel</i><div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+            '<div class="row"><span>Programa</span><select class="programa"><option value="normal">Normal</option>' +
+            '</select></div><div class="row"><div class="checkbox"><label>Arrumar após finalizar: <input type="checkbox" value="arrumar"></label></div></div></div></div></div>');
+            $("#popup-close").click(function(){
+                $("#popup-close").unbind('click');
+                $(".pop-up").remove();
+            });
+            $("#state").bootstrapSwitch('size', 'mini');
+            if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+            $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+                localStorage.setItem('loicaState', state);
+            });
+    });
 }
 
 $(document).ready(function(){main()});
--- código/sala.js
+++ código/sala.js
@@ -16,10 +16,10 @@ function main(){
         date.setSeconds(date.getSeconds() + 1);
         updateTimeUI();
     }, 1000);
-    $("#tv").click(function(){console.log("test");
+    $("#tv").click(function(){
         $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Televisão' +
             '</h1><i class="material-icons close" id="popup-close">' +
-                    'cancel</i><div class="row"><span>Estado: </span>OFF<label class="switch"><input type="checkbox"><span class="slider"></span></label>ON</span></div>' +
+                    'cancel</i><div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
             '<div class="row"><span>Canal:</span><select class="canal"><option value="89">89</option>' +
             '<option value="88">88</option><option value="87">87</option></select></div></div></div></div>');
                 $("#popup-close").click(function(){
@@ -27,6 +27,13 @@ function main(){
                     $(".pop-up").remove();
                 });
         $(".canal").select2();
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('tvState') !== undefined) {
+            $("#state").bootstrapSwitch('state', localStorage.getItem('tvState'));
+        }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('tvState', state);
+        });
     });
 
 }
