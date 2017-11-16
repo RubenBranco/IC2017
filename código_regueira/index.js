3cd112c497be4bc5707cb4a9ee2859716ccd137a
João Regueira <32177995+JoaoRegueira@users.noreply.github.com>
2017-11-16 17:57:24 +0000

    Regueira

---
+++ código_regueira/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var date;
+
+
+function updateTimeUI(){
+    $("#dateDay").text(date.getDay()+"  /");
+    $("#dateMonth").text(date.getMonth()+"  /");
+    $("#dateYear").text(date.getFullYear());
+    $("#dateTime").text(date.toLocaleTimeString());
+}
+
+function main(){
+    date = new Date();
+    updateTimeUI();
+    setInterval(function(){
+        date.setSeconds(date.getSeconds() + 1);
+        updateTimeUI();
+    }, 1000);
+}
+
+$(document).ready(function(){main()});
