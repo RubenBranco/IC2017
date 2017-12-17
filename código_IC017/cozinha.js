bb06d3950a3317abdc0eb4c4dbc458346d393e98
GoncaloBISantos <32179963+GoncaloBISantos@users.noreply.github.com>
2017-12-17 21:46:19 +0000

    Add files via upload

--- código_IC017/cozinha.js
+++ código_IC017/cozinha.js
@@ -40,6 +40,177 @@ function main(){
             localStorage.setItem('loicaState', state);
         });
     });
+    $("#foodInv").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Inventário de comida' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Alimentos</span><select class="inventario"><option value="massa">Massa</option>' +
+                              '<option value="arroz">Arroz</option><option value="chocolate">Chocolate</option></select>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+    });
+
+    $("#fogao").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Fogão' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Acender bico</span><select class="programa"><option value="normal">Bico 1</option>' +
+                              '</select><div class="row"><span>Temperatura</span><select class="tempo"><option value="média">Média</option>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#roupa").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Máquina de Lavar roupa' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Programa</span><select class="programa"><option value="normal">Lavar</option>' +
+                              '</select><div class="row"><span>Temperatura</span><select class="programa"><option value="normal">180º</option>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#fridge").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Frigorífico' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '</select><div class="row"><span>Nível de frio</span><select class="programa"><option value="normal">4</option>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#roupaSec").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Máquina de secar Roupa' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Programa</span><select class="programa"><option value="normal">Secagem total</option>' +
+                              '</select></div><div class="row"><div class="checkbox"><label><input type="checkbox" value="arrumar">Arrumar após finalizar: </label></div></div></div></div></div>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#micro").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Microondas' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Watts</span><select class="programa"><option value="normal">850W</option>' +
+                              '</select><div class="row"><span>Tempo</span><select class="programa"><option value="normal">10 minutos</option>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#cafe").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Máquina de cafe' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Servir café</span><select class="programa"><option value="normal">Curto</option>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#forno").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Forno' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+                              '<div class="row"><span>Temperatura</span><select class="programa"><option value="normal">Alta</option>' +
+                              '</select><div class="row"><span>Tempo</span><select class="programa"><option value="normal">30 min</option>');
+
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch('size', 'mini');
+        if (localStorage.getItem('loicaState') !== undefined) {
+                $("#state").bootstrapSwitch('state', localStorage.getItem('loicaState'));
+            }
+        $("#state").on('switchChange.bootstrapSwitch', function(event, state){
+            localStorage.setItem('loicaState', state);
+        });
+    });
+
+
+    $("#jantar").click(function(){
+    $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Preparar jantar' +
+                            '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                            "<div class='row'><label>Número de pessoas <input type='text' class='form-control mesa'></label></div></div></div></div>");
+    $("#popup-close").click(function(){
+        $("#popup-close").unbind('click');
+        $(".pop-up").remove();
+      });
+    });
+
+    $("#refeicoes").click(function(){
+    $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Refeições' +
+                            '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                            '<div class="row"><span>Prato</span><select class="programa"><option value="normal">Bitoque</option>' +
+                            "</select><div class='row'><label>Número de pessoas <input type='text' class='form-control mesa'></label></div></div></div></div>");
+    $("#popup-close").click(function(){
+        $("#popup-close").unbind('click');
+        $(".pop-up").remove();
+      });
+    });
+
     $("#mesa").click(function(){
        $(".ui-wrapper").append("<div class='pop-up'><div class='content'><div class='popup-container'><h1>Meter a mesa" +
            "</h1><button class='btn-primary btn-md' id='popup-close'>Fechar</button>" +
@@ -51,7 +222,6 @@ function main(){
                 $('#myModal').modal('show');
             }
         });
-        
     });
 }
 
--- código_IC017/quarto.js
+++ código_IC017/quarto.js
@@ -36,8 +36,107 @@ function main() {
     $("#logoutA").click(function () {
         location.replace("login.html");
     });
-    //começaa fazer os pop ups aqui
-    
+    $("#chao").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Chão' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Limpeza</span><select class="inventario"><option value="massa">Mínima</option>' +
+                              '<option value="arroz">Total</option><option value="chocolate">Parcial</option></select>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+    });
+
+    $("#banheira").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Chão' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Modo de banho</span><select class="inventario"><option value="massa">Relaxante</option>' +
+                              '<option value="arroz">Rápido</option><option value="chocolate">Normal</option></select>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+    });
+
+    $("#lights").click(function () {
+        $(".ui-wrapper").append("<div class='pop-up'><div class='content'><div class='popup-container'><h1>Luzes</h1>" +
+            "<button class=\"btn-primary btn-md\" id=\"popup-close\">Fechar</button>" +
+            '<div class="row"><span>Estado: </span><input type="checkbox" id="state"></div>' +
+            "<div class='slider-container container'><span>Intensidade</span><input type='range' min='0' max='100' value='50' id='lightRange'>" +
+            "</div><div id='rangeControl'><i class='material-icons' style='cursor:pointer'>remove_circle</i><input type='number' style='width:50px;' max='100' min='0'><i style='cursor:pointer' class='material-icons'>add_circle</i></div></div></div></div>");
+        if (localStorage.getItem('livingRoomLightState') !== undefined) {
+            $("#lightRange").val(localStorage.getItem('livingRoomLightState'));
+        }
+        $("#popup-close").click(function () {
+            $("#popup-close").unbind("click");
+            localStorage.setItem("livingRoomLightState", $("#lightRange").val());
+            $(".pop-up").remove();
+        });
+        $("#state").bootstrapSwitch("size", "mini");
+        stateSetter("state", "airconState");
+        stateHandler("state", "airconState");
+        $("#rangeControl input").val($("#temp").val());
+        $("#rangeControl input").change(function(){
+            $("#temp").val($(this).val());
+        });
+        $("#rangeControl input").val($("#lightRange").val());
+        $("#rangeControl input").change(function(){
+            $("#lightRange").val($(this).val());
+        });
+        $("#rangeControl i").click(function(){
+            var which = $(this).text();
+            var value = which === "add_circle" ? 1 : -1;
+            var op = Number($("#rangeControl input").val()) + value
+            if (op >= 0 && op <= 100) {
+                $("#rangeControl input").val(String(op));
+                $("#lightRange").val(String(op));
+            }
+        });
+    });
+
+    $("#aircon").click(function () {
+        $(".ui-wrapper").append("<div class='pop-up'><div class='content'><div class='popup-container'>" +
+            "<h1>Ar Condicionado</h1><button class=\"btn-primary btn-md\" id=\"popup-close\">Fechar</button>" +
+            "<div class='row'>Estado: <input type='checkbox' id='state'></div>" +
+            "<div class='row slider-container container'><span>Temperatura</span><input type='range' min='16' max='30' value='24' id='temp'>" +
+            "</div><div id='rangeControl'><i class='material-icons' style='cursor:pointer'>remove_circle</i><input type='number' style='width:50px;' max='30' min='16'><i style='cursor:pointer' class='material-icons'>add_circle</i></div></div></div></div>");
+        $("#popup-close").click(function () {
+            $("#popup-close").unbind("click");
+            localStorage.setItem("temperatureState", $("#temp").val());
+            $(".pop-up").remove();
+        });
+        if (localStorage.getItem("temperatureState") !== undefined) {
+            $("#temp").val(localStorage.getItem("temperatureState"));
+        }
+        $("#state").bootstrapSwitch("size", "mini");
+        stateSetter("state", "airconState");
+        stateHandler("state", "airconState");
+        $("#rangeControl input").val($("#temp").val());
+        $("#rangeControl input").change(function(){
+            $("#temp").val($(this).val());
+        });
+        $("#rangeControl i").click(function(){
+            var which = $(this).text();
+            var value = which === "add_circle" ? 1 : -1;
+            var op = Number($("#rangeControl input").val()) + value
+            if (op >= 16 && op <= 30) {
+                $("#rangeControl input").val(String(op));
+                $("#temp").val(String(op));
+            }
+        });
+    });
+
+    $("#musica").click(function(){
+      $(".ui-wrapper").append('<div class="pop-up"><div class="content"><div class="popup-container"><h1>Música' +
+                              '</h1><button class="btn-primary btn-md" id="popup-close">Fechar</button>' +
+                              '<div class="row"><span>Playlist</span><select class="inventario"><option value="hiphop">Hip hop</option>' +
+                              '<option value="jazz">Jazz</option><option value="soul">Soul</option></select>');
+      $("#popup-close").click(function(){
+          $("#popup-close").unbind('click');
+          $(".pop-up").remove();
+        });
+    });
+
 }
 
 $(document).ready(function () {
