f73ab93ebf72ca205c9007d99ea9e26f9de0067e
Joao Regueira <joao.b.regueira@gmail.com>
2017-12-17 19:07:59 +0000

    SantosWorks INC.

---
+++ código_IC017/quarto.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var date;
+
+function updateTimeUI() {
+    $("#dateDay").text(date.getDate() + ' /');
+    $("#dateMonth").text(date.getMonth() + 1 + ' /');
+    $("#dateYear").text(date.getFullYear());
+    $("#dateTime").text(date.toLocaleTimeString());
+}
+
+function stateSetter(id, key) {
+    if (localStorage.getItem(key) !== undefined) {
+        if (localStorage.getItem(key) === "true") {
+            $("#" + id).bootstrapSwitch('state', true);
+        }
+        else {
+            $("#" + id).bootstrapSwitch('state', false);
+        }
+    }
+}
+
+function stateHandler(id, stateKey) {
+    $("#" + id).on('switchChange.bootstrapSwitch', function (event, state) {
+        localStorage.setItem(stateKey, String(state));
+    });
+}
+
+function main() {
+    date = new Date();
+    updateTimeUI();
+    setInterval(function () {
+        date.setSeconds(date.getSeconds() + 1);
+        updateTimeUI();
+    }, 1000);
+    $("#logoutA").click(function () {
+        location.replace("login.html");
+    });
+    //começaa fazer os pop ups aqui
+    
+}
+
+$(document).ready(function () {
+    main()
+});
